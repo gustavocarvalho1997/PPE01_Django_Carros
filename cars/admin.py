95cf41944d5ffd86baa0bfee2997faa1a84d3fd9
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-01-19 18:36:37 -0300

    add BrandAdmin

--- cars/admin.py
+++ cars/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 # Register your models here.
 # Classes de configuração do admin
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model', 'brand')
 
 
 # Registra o model no admin
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
