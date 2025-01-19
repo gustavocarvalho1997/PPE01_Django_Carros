fd52d6334fdcead1b3852ddbcfc2db6517f3bc60
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-01-19 17:29:14 -0300

    projeto django iniciado e criado model car

---
+++ cars/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Car(models.Model):
+    # Atributos
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
