3d6af8f251f512520ba22ac2125b3d5590bb0268
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-01-19 18:27:10 -0300

    criado model Brand e relacao com Car

--- cars/models.py
+++ cars/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    # Atributos
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    # Método para retornar o nome da marca
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     # Atributos
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
