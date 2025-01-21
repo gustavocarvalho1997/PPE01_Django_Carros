294ef6d5f87f2b716af32e97f59660ff6242d020
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-01-21 17:23:04 -0300

    query para pegar a lista de carros e disponibilizar na view

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
+    cars = Car.objects.all()
     return render(
         request, 
         'cars.html',
-        {'cars': {'model': 'Model S', 'make': 'Tesla', 'year': 2021}}
+        {'cars': cars}
     )
