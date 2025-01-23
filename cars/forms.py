dc71d0c5f0ad09e55340237800eb8b26d84d5015
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-01-23 03:46:55 -0300

    feita a funcao para salvar no banco com o form

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from cars.models import Brand
+from cars.models import Car
 
 class CarForm(forms.Form):
     model = forms.CharField(label='Modelo', max_length=200)
@@ -9,3 +10,16 @@ class CarForm(forms.Form):
     plate = forms.CharField(label='Placa', max_length=10, required=False)
     value = forms.DecimalField(label='Valor', max_digits=10, decimal_places=2, required=False)
     photo = forms.ImageField(label='Foto', required=False)
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            value = self.cleaned_data['value'],
+            photo = self.cleaned_data['photo']
+        )
+        car.save()
+        return car
