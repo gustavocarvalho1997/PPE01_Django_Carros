3e0251d28563a2bcbbb9f4ea442bf8cfe884d3ea
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-01-23 23:52:37 -0300

    migrando para ModelForm

--- cars/forms.py
+++ cars/forms.py
@@ -1,25 +1,8 @@
 from django import forms
 from cars.models import Brand
 from cars.models import Car
-
-class CarForm(forms.Form):
-    model = forms.CharField(label='Modelo', max_length=200)
-    brand = forms.ModelChoiceField(label='marca', queryset=Brand.objects.all())
-    factory_year = forms.IntegerField(label='Ano de Fabricação', required=False)
-    model_year = forms.IntegerField(label='Ano do Modelo', required=False)
-    plate = forms.CharField(label='Placa', max_length=10, required=False)
-    value = forms.DecimalField(label='Valor', max_digits=10, decimal_places=2, required=False)
-    photo = forms.ImageField(label='Foto', required=False)
-
-    def save(self):
-        car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            photo = self.cleaned_data['photo']
-        )
-        car.save()
-        return car
+    
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
