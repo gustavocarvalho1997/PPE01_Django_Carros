f8ad601e34dc5326e30ad45d89a194b8630c9d98
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-01-30 17:10:36 -0300

    implementando login required

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,8 @@
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.  
@@ -15,7 +17,8 @@ class CarsListView(ListView):
         if search:
             cars = cars.filter(model__icontains=search)
         return cars
-  
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
@@ -26,6 +29,7 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -34,6 +38,7 @@ class CarUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
