cf86b0d7083dc94de3eefad991302d91a4887acf
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-02-06 00:45:55 +0000

    allowed

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-fecn*nz3+_%!@*3fx%&r+45tn+vl*()v!89p5y=4!gbi0&)ama
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
